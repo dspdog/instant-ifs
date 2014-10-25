4d4e0fd58b993ab758e546982bbdd0d6a138340f
mrpawel <pawel745@gmail.com>
2014-10-24 23:21:33 -0400

    improved meshlab script

--- src/ifs/ifsEvolution.java
+++ src/ifs/ifsEvolution.java
@@ -29,7 +29,7 @@ public class ifsEvolution {
 
         System.out.println("start of evolution...");
 
-        int totalSibs = 20;
+        int totalSibs = 2;
         int totalGens = 10000;
 
         Random rnd = new Random();
